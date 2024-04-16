7929fc05a0f5841b3f05b66b031ab93b23478322
Cole Manel <csmanel@gmail.com>
2024-04-15 21:41:30 -0400

    start wordle

--- wordle/src/App.js
+++ wordle/src/App.js
@@ -3,3 +3,16 @@ function App() {
 }
 
 export default App;
+
+/*
+
+things we need to take care of 
+
+a guess is a string of 5 characters 
+  --an array of past guesses, containing objects {Key: 'a', val: color}
+    --the object will contain the letter and what color it is
+  --a way to keep track of current guess 
+  
+
+  
+*/
--- wordle/src/components/GuessInput.jsx
+++ wordle/src/components/GuessInput.jsx
@@ -1,35 +1,7 @@
 import React, { useState } from 'react';
 
-const GuessInput = ({ onSubmit }) => {
-  const [guess, setGuess] = useState('');
-
-  const handleChange = (e) => {
-    setGuess(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(guess);
-    setGuess('');
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Take a guess!
-        <input
-          type="text"
-          value={guess}
-          onChange={handleChange}
-          maxLength={5}
-          minLength={5}
-          pattern="[A-za-z]{5}"
-          required
-        />
-      </label>
-      <button type="submit">Guess</button>
-    </form>
-  );
+const GuessInput = () => {
+  return <div></div>;
 };
 
 export default GuessInput;
