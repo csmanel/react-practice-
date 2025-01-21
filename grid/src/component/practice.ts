fac7b2db329e894a25edb95ffe7d3f30a024c47a
Cole Manel <csmanel@gmail.com>
2025-01-21 18:57:53 -0500

    ts best time

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -9,3 +9,19 @@ function twoSum(nums: number[], target: number): number[] | undefined {
   }
   return undefined;
 }
+class Solution {
+  maxProfit(prices: number[]): number {
+    let buy: number = prices[0];
+    let profit: number = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+      if (prices[i] < buy) {
+        buy = prices[i];
+      } else if (prices[i] - buy > profit) {
+        profit = prices[i] - buy;
+      }
+    }
+
+    return profit;
+  }
+}
