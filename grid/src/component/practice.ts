ea9d2578e90d35585c7e7cbb290e80e351e7fb02
Cole Manel <csmanel@gmail.com>
2025-01-23 16:58:21 -0500

    add two numbers ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -25,3 +25,34 @@ class Solution {
     return profit;
   }
 }
+
+Copyclass ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+}
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const dummy = new ListNode(0);
+  let current = dummy;
+  let carry = 0;
+
+  while (l1 || l2 || carry) {
+      const x = l1 ? l1.val : 0;
+      const y = l2 ? l2.val : 0;
+      
+      const total = x + y + carry;
+      carry = Math.floor(total / 10);
+      
+      current.next = new ListNode(total % 10);
+      current = current.next;
+      
+      l1 = l1 ? l1.next : null;
+      l2 = l2 ? l2.next : null;
+  }
+  
+  return dummy.next;
+}
