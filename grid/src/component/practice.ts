2bed0f899c143900244d02314c7b90ff0ada244a
Cole Manel <csmanel@gmail.com>
2025-01-23 16:59:35 -0500

    longeest length in ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -53,6 +53,24 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
       l1 = l1 ? l1.next : null;
       l2 = l2 ? l2.next : null;
   }
-  
+
   return dummy.next;
+}
+
+function lengthOfLongestSubstring(s: string): number {
+  const charSet = new Set<string>();
+  let maxLength = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+      while (charSet.has(s[right])) {
+          charSet.delete(s[left]);
+          left++;
+      }
+      
+      charSet.add(s[right]);
+      maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
 }
