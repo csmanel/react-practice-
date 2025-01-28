7df752629fb307e6662beadfc2e1c7e3fd8c86a1
Cole Manel <csmanel@gmail.com>
2025-01-28 18:35:07 -0500

    container with most water ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -299,3 +299,21 @@ class Solution {
       return dp[m][n];
   }
 }
+
+function maxArea(height: number[]): number {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+      maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+      if (height[left] < height[right]) {
+          left++;
+      } else {
+          right--;
+      }
+  }
+
+  return maxArea;
+}
