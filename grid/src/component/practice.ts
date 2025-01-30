9c98dc562ba1b2445aa5726ff6303b62e8555233
Cole Manel <csmanel@gmail.com>
2025-01-30 08:49:20 -0500

    into to roman ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -317,3 +317,22 @@ function maxArea(height: number[]): number {
 
   return maxArea;
 }
+
+const intToRoman = (num: number): string => {
+  const valueSymbols: [number, string][] = [
+      [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+      [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+      [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+  ];
+  
+  let res: string = '';
+
+  for (const [value, symbol] of valueSymbols) {
+      if (num === 0) break;
+      const count: number = Math.floor(num / value);
+      res += symbol.repeat(count);
+      num -= count * value;
+  }
+
+  return res;
+};
