db546499db4d53850b9f65550e62f6a1f595f60b
Cole Manel <csmanel@gmail.com>
2025-01-31 09:26:06 -0500

    three sum ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -371,3 +371,36 @@ class Solution {
       return ans;
   }
 }
+
+function threeSum(nums: number[]): number[][] {
+  let res: number[][] = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+      if (i > 0 && nums[i] === nums[i - 1]) {
+          continue;
+      }
+
+      let j = i + 1;
+      let k = nums.length - 1;
+
+      while (j < k) {
+          let total = nums[i] + nums[j] + nums[k];
+
+          if (total > 0) {
+              k--;
+          } else if (total < 0) {
+              j++;
+          } else {
+              res.push([nums[i], nums[j], nums[k]]);
+              j++;
+
+              while (j < k && nums[j] === nums[j - 1]) {
+                  j++;
+              }
+          }
+      }
+  }
+  return res;
+}
+
