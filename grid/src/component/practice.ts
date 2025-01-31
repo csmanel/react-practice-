c03370af1d72c7ace7f735593578ab2eb497b924
Cole Manel <csmanel@gmail.com>
2025-01-31 09:25:47 -0500

    longest common prefix ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -354,3 +354,20 @@ const romanToInt = (s: string): number => {
 
   return res + roman[s[s.length - 1]];
 };
+
+class Solution {
+  longestCommonPrefix(v: string[]): string {
+      let ans = "";
+      v.sort(); 
+      let first = v[0];
+      let last = v[v.length - 1];
+
+      for (let i = 0; i < Math.min(first.length, last.length); i++) {
+          if (first[i] !== last[i]) {
+              return ans;
+          }
+          ans += first[i];
+      }
+      return ans;
+  }
+}
