d4ec7e59b732c16b2867f9957001e39cdae8a1cd
Cole Manel <csmanel@gmail.com>
2025-02-05 16:39:21 -0500

    merge two sorted lists ts

--- grid/src/component/practice.ts
+++ grid/src/component/practice.ts
@@ -557,3 +557,33 @@ const isValid = (s: string): boolean => {
 
   return stack.length === 0;
 };
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.next = next === undefined ? null : next;
+  }
+}
+
+const mergeTwoLists = (list1: ListNode | null, list2: ListNode | null): ListNode | null => {
+  let dummy = new ListNode();
+  let cur = dummy;
+
+  while (list1 && list2) {
+      if (list1.val > list2.val) {
+          cur.next = list2;
+          list2 = list2.next;
+      } else {
+          cur.next = list1;
+          list1 = list1.next;
+      }
+      cur = cur.next;
+  }
+
+  cur.next = list1 || list2;
+
+  return dummy.next;
+};
+
